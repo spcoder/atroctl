e2af994d21f51f9a8ea4791407bc4db2346d8bc8
Cody Frederick <cody.frederick@outlook.com>
2021-01-28 07:31:20 -0600

    checkpoint

--- cmd/deploy.go
+++ cmd/deploy.go
@@ -1,9 +1,14 @@
 package cmd
 
 import (
+	"bytes"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -13,29 +18,164 @@ var (
 	strategy string
 )
 
-// deployCmd represents the deploy command
 var deployCmd = &cobra.Command{
 	Use:   "deploy",
-	Short: "deploy to atrocity",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "deploy to Atrocity",
+	Long: `Deploy secrets and functions to Atrocity.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+Strategies:
+When deploying you'll need to choose a strategy.
+* bluegreen = rotates between blue and green deployments
+* gitrev    = uses 'git rev-parse HEAD' as the deployment id
+* uuid      = uses a random uuid as the deployment id
+
+Secrets:
+Any environment variable that starts with ATROCITY_ will be deployed to Atrocity.
+The secret will be available to atrocity functions without the ATROCITY_. For example,
+ATROCITY_PG_CONNECTION will be available as PG_CONNECTION.
+
+Examples:
+	# deploys all *.js files in the current directory to http://localhost:9090 using the bluegreen strategy
+	atroctl -d .
+
+	# deploys all *.js files recursively in ~/dev/project/server to http://example.com using the latest revision number in the current directory
+	atroctl -u http://example.com -d ~/dev/project/server -s gitrev`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("deploy called")
-		fmt.Printf("url: %s\n", url)
-		fmt.Printf("strategy: %s\n", strategy)
-		files, err := glob(dir, ".js")
-		if err != nil {
-			fmt.Printf("crap: %s", err)
-			return
+		var err error
+		p("atroctl", "starting deployment to %s\n", url)
+		p("strategy", "using strategy %s\n", strategy)
+		switch strategy {
+		case "bluegreen":
+			err = bluegreen()
+		default:
+			err = fmt.Errorf("strategy (%s) not supported", strategy)
 		}
-		for _, file := range files {
-			fmt.Println(file)
+		if err != nil {
+			p("error", "%s\n", err)
 		}
 	},
+}
+
+func p(key, msg string, args ...interface{}) {
+	if key == "" {
+		fmt.Printf(msg, args...)
+		return
+	}
+	fmt.Printf("%10s: %s", strings.ToUpper(key), fmt.Sprintf(msg, args...))
+}
+
+func httpPut(url, contentType string, body io.Reader) (*http.Response, error) {
+	req, err := http.NewRequest(http.MethodPut, url, body)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", contentType)
+	client := &http.Client{}
+	return client.Do(req)
+}
+
+func bluegreen() error {
+	deployId, err := getDeployId()
+	if err != nil {
+		return fmt.Errorf("error getting deploy id: %w", err)
+	}
+	if deployId == "blue" {
+		deployId = "green"
+	} else {
+		deployId = "blue"
+	}
+	return deploy(deployId)
+}
+
+func deploy(deployId string) error {
+	p(strategy, "deploying to %s\n", deployId)
+	err := deploySecrets(deployId)
+	if err != nil {
+		return err
+	}
+	err = deployFunctions(deployId)
+	if err != nil {
+		return err
+	}
+	err = activateDeployment(deployId)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func getDeployId() (string, error) {
+	resp, err := http.Get(fmt.Sprintf("%s/deploy", url))
+	if err != nil {
+		return "", err
+	}
+	defer func() { _ = resp.Body.Close() }()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
+
+func deploySecrets(deployId string) error {
+	p("secrets", "starting to deploy secrets\n")
+	for _, v := range envvars() {
+		p("secrets", "deploying %s", v.key)
+		resp, err := httpPut(fmt.Sprintf("%s/deploy/%s/secret/%s", url, deployId, v.key), "text/plain", strings.NewReader(v.value))
+		if err != nil {
+			return fmt.Errorf("error deploying secret (%s): %w", v.key, err)
+		}
+		if resp.StatusCode == http.StatusNoContent {
+			p("", " [OK]\n")
+		} else {
+			p("", " [%d]\n", resp.StatusCode)
+			return fmt.Errorf("failed to deploy secret (%s)", v.key)
+		}
+	}
+	p("secrets", "successfully deployed\n")
+	return nil
+}
+
+func deployFunctions(deployId string) error {
+	p("functions", "starting to deploy functions\n")
+	files, err := glob(dir, ".js")
+	if err != nil {
+		return fmt.Errorf("error globbing files: %w", err)
+	}
+	for _, f := range files {
+		p("functions", "deploying file %s", f)
+		contents, err := ioutil.ReadFile(f)
+		if err != nil {
+			return fmt.Errorf("error reading file (%s): %w", f, err)
+		}
+		basename := filepath.Base(f)
+		resp, err := httpPut(fmt.Sprintf("%s/deploy/%s/function/%s", url, deployId, basename), "text/plain", bytes.NewReader(contents))
+		if err != nil {
+			return fmt.Errorf("error deploying function (%s): %w", f, err)
+		}
+		if resp.StatusCode == http.StatusNoContent {
+			p("", " [OK]\n")
+		} else {
+			p("", " [%d]\n", resp.StatusCode)
+			return fmt.Errorf("failed to deploy function (%s)", f)
+		}
+	}
+	p("functions", "successfully deployed\n")
+	return nil
+}
+
+func activateDeployment(deployId string) error {
+	p("activate", "starting to activate %s\n", deployId)
+	resp, err := http.Post(fmt.Sprintf("%s/deploy/%s/activate", url, deployId), "text/plain", nil)
+	if err != nil {
+		return fmt.Errorf("error activating deployment: %w", err)
+	}
+	if resp.StatusCode == http.StatusNoContent {
+		p("activate", "successfully activated %s\n", deployId)
+	} else {
+		return fmt.Errorf("failed to activate deployment: %w", err)
+	}
+	return nil
 }
 
 func glob(dir string, ext string) ([]string, error) {
@@ -49,18 +189,25 @@ func glob(dir string, ext string) ([]string, error) {
 	return files, err
 }
 
+type keyval struct {
+	key   string
+	value string
+}
+
+func envvars() []keyval {
+	result := make([]keyval, 0)
+	for _, e := range os.Environ() {
+		pair := strings.SplitN(e, "=", 2)
+		if strings.HasPrefix(pair[0], "ATROCITY_") {
+			key := strings.Replace(pair[0], "ATROCITY_", "", 1)
+			result = append(result, keyval{key, pair[1]})
+		}
+	}
+	return result
+}
+
 func init() {
 	deployCmd.Flags().StringVarP(&dir, "dir", "d", ".", "the directory to scan for js files")
-	deployCmd.Flags().StringVarP(&strategy, "strategy", "s", "bluegreen", "the deployment strategy (bluegreen, githead)")
+	deployCmd.Flags().StringVarP(&strategy, "strategy", "s", "bluegreen", "the deployment strategy (bluegreen, gitrev, uuid)")
 	rootCmd.AddCommand(deployCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// deployCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// deployCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- cmd/root.go
+++ cmd/root.go
@@ -7,25 +7,18 @@ import (
 )
 
 var (
-	//cfgFile string
-	url string
+	url       string
+	apikey    string
+	secretkey string
 )
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:     "atroctl",
 	Version: "1.0.0",
-	Short:   "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short:   "atroctl controls Atrocity instances",
+	Long:    `The atroctl command lets you control instances of Atrocity.`,
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
@@ -34,42 +27,7 @@ func Execute() {
 }
 
 func init() {
-	//cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
 	rootCmd.PersistentFlags().StringVarP(&url, "url", "u", "http://localhost:9090", "the url to atrocity")
-	//rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.atroctl.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	//rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.PersistentFlags().StringVarP(&apikey, "apikey", "k", "", "the api key")
+	rootCmd.PersistentFlags().StringVarP(&secretkey, "secretkey", "x", "", "the api secret key")
 }
-
-//// initConfig reads in config file and ENV variables if set.
-//func initConfig() {
-//	if cfgFile != "" {
-//		// Use config file from the flag.
-//		viper.SetConfigFile(cfgFile)
-//	} else {
-//		// Find home directory.
-//		home, err := homedir.Dir()
-//		if err != nil {
-//			fmt.Println(err)
-//			os.Exit(1)
-//		}
-//
-//		// Search config in home directory with name ".atroctl" (without extension).
-//		viper.AddConfigPath(home)
-//		viper.SetConfigName(".atroctl")
-//	}
-//
-//	viper.AutomaticEnv() // read in environment variables that match
-//
-//	// If a config file is found, read it in.
-//	if err := viper.ReadInConfig(); err == nil {
-//		fmt.Println("Using config file:", viper.ConfigFileUsed())
-//	}
-//}
